b419ac53394f52c1522fcb0fb500682033e0576d
nmmmk <mnagayoshi75@gmail.com>
2023-02-18 16:12:25 +0900

    add: ベースプロジェクト追加

---
+++ SwiftLintSample/SwiftLintSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintSampleApp.swift
+//  SwiftLintSample
+//
+//  Created by 永芳元治 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
