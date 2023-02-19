a474bcadc69d82dad4f39e19036f4336cc169b12
nmmmk <mnagayoshi75@gmail.com>
2023-02-19 11:13:45 +0900

    add: danger-swiftパッケージ

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "BuildTools",
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", exact: "0.49.1")
+        .package(url: "https://github.com/realm/SwiftLint", exact: "0.49.1"),
+        .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "BuildTools", path: "")
