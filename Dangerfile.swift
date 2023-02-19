d46d6a2be3a7a98a7ce0a9f70b8b133f38c8f7c9
nmmmk <mnagayoshi75@gmail.com>
2023-02-19 11:44:37 +0900

    chore: DangerSwiftの定義修正

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -3,13 +3,6 @@ import Foundation
 
 let danger = Danger()
 
-// fileImport: DangerfileExtensions/ChangelogCheck.swift
-//checkChangelog()
-
-if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
-    warn("Big PR, try to keep changes smaller if you can")
-}
-
 let swiftFilesWithCopyright = danger.git.createdFiles.filter {
     $0.fileType == .swift
         && danger.utils.readFile($0).contains("//  Created by")
